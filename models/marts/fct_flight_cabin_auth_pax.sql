4d290e5578c474798e40edf95739d243a4b042c7
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2024-07-24 15:42:53 +0000

    modified

--- models/marts/fct_flight_cabin_auth_pax.sql
+++ models/marts/fct_flight_cabin_auth_pax.sql
@@ -4,27 +4,79 @@
     on_schema_change='fail'
 ) }}
 
-{% if is_incremental() %}
-
-WITH max_timestamp AS (
-    SELECT COALESCE(MAX(sabre_transaction_timestamp), '1900-01-01') AS max_timestamp
-    FROM {{ this }}
+WITH authorized_pax AS (
+    SELECT 
+        airline_code,
+        flight_number,
+        departure_date,
+        departure_airport,
+        flight_key,
+        first_class_authorized_pax,
+        business_class_authorized_pax,
+        premium_economy_authorized_pax,
+        economy_authorized_pax,
+        sabre_transaction_timestamp
+    FROM {{ ref('int_authorized_pax_qty') }}
+    {% if is_incremental() %}
+    WHERE sabre_transaction_timestamp > (SELECT COALESCE(MAX(sabre_transaction_timestamp), '1900-01-01') FROM {{ this }})
+    {% endif %}
 ),
 
-authorized_pax AS (
-    SELECT a.*
-    FROM {{ ref('int_authorized_pax_qty') }} a
-    CROSS JOIN max_timestamp
-    WHERE a.sabre_transaction_timestamp > max_timestamp.max_timestamp
+unpivoted AS (
+    SELECT
+        airline_code,
+        flight_number,
+        departure_date,
+        departure_airport,
+        flight_key,
+        'F' as cabin_class,
+        first_class_authorized_pax as authorized_pax_qty,
+        sabre_transaction_timestamp
+    FROM authorized_pax
+    WHERE first_class_authorized_pax IS NOT NULL
+
+    UNION ALL
+
+    SELECT
+        airline_code,
+        flight_number,
+        departure_date,
+        departure_airport,
+        flight_key,
+        'C' as cabin_class,
+        business_class_authorized_pax as authorized_pax_qty,
+        sabre_transaction_timestamp
+    FROM authorized_pax
+    WHERE business_class_authorized_pax IS NOT NULL
+
+    UNION ALL
+
+    SELECT
+        airline_code,
+        flight_number,
+        departure_date,
+        departure_airport,
+        flight_key,
+        'W' as cabin_class,
+        premium_economy_authorized_pax as authorized_pax_qty,
+        sabre_transaction_timestamp
+    FROM authorized_pax
+    WHERE premium_economy_authorized_pax IS NOT NULL
+
+    UNION ALL
+
+    SELECT
+        airline_code,
+        flight_number,
+        departure_date,
+        departure_airport,
+        flight_key,
+        'Y' as cabin_class,
+        economy_authorized_pax as authorized_pax_qty,
+        sabre_transaction_timestamp
+    FROM authorized_pax
+    WHERE economy_authorized_pax IS NOT NULL
 )
-
-{% else %}
-
-WITH authorized_pax AS (
-    SELECT * FROM {{ ref('int_authorized_pax_qty') }}
-)
-
-{% endif %}
 
 SELECT
     airline_code,
@@ -35,49 +87,4 @@ SELECT
     cabin_class,
     authorized_pax_qty,
     sabre_transaction_timestamp
-FROM (
-    SELECT 
-        airline_code,
-        flight_number,
-        departure_date,
-        departure_airport,
-        flight_key,
-        'F' as cabin_class,
-        first_class_authorized_pax as authorized_pax_qty,
-        sabre_transaction_timestamp
-    FROM authorized_pax
-    UNION ALL
-    SELECT 
-        airline_code,
-        flight_number,
-        departure_date,
-        departure_airport,
-        flight_key,
-        'C' as cabin_class,
-        business_class_authorized_pax as authorized_pax_qty,
-        sabre_transaction_timestamp
-    FROM authorized_pax
-    UNION ALL
-    SELECT 
-        airline_code,
-        flight_number,
-        departure_date,
-        departure_airport,
-        flight_key,
-        'W' as cabin_class,
-        premium_economy_authorized_pax as authorized_pax_qty,
-        sabre_transaction_timestamp
-    FROM authorized_pax
-    UNION ALL
-    SELECT 
-        airline_code,
-        flight_number,
-        departure_date,
-        departure_airport,
-        flight_key,
-        'Y' as cabin_class,
-        economy_authorized_pax as authorized_pax_qty,
-        sabre_transaction_timestamp
-    FROM authorized_pax
-)
-WHERE authorized_pax_qty IS NOT NULL
+FROM unpivoted
